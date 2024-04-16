d50193e32e1086a2f17c7f0cb349f0f081a90a0f
Cha-Oliveira <charlespiedade@gmail.com>
2024-04-16 19:09:43 -0300

    Incremento do Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,11 @@
-7
+import { useState } from 'react'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+    </button>
+  )
+}
